6b2788bb52f11b87a356610c39e56c5978fd98ec
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-11-14 15:00:37 -0300

    funcion agregada para obtener el perfil del usuario

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 export const Navbar = () => {
   // TODO: Obtener datos del usuario desde /api/profile
   // TODO: Implementar función handleLogout con POST a /api/logout usando credentials: 'include'
   // TODO: Después del logout exitoso, redireccionar a /login
   // TODO: Manejar errores apropiadamente
+  const [userName, setUserName] = useState("");
+  const navigate = useNavigate();
 
-  const userName = "Usuario"; // TODO: Reemplazar con el nombre real del usuario obtenido de /api/profile
+  // se obtiene datos del user
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/profile", {
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error("no autenticado");
+        }
+
+        const data = await res.json();
+        setUserName(data.name); // muestro solo el nombre
+      } catch (error) {
+        navigate("/login");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleLogout = () => {};
 
   return (
     <nav className="bg-gray-900 text-white h-16 left-0 right-0 shadow-lg sticky top-0 z-50">
@@ -18,9 +45,7 @@ export const Navbar = () => {
           </span>
 
           <button
-            onClick={() => {
-              // TODO: Implementar handleLogout aquí
-            }}
+            onClick={handleLogout} // TODO: Implementar handleLogout aquí
             className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded transition-colors font-medium"
           >
             Cerrar Sesión
